96fbdf873352d6de73df0b63130f7937e1cf62dd
leo le leu <leohrc76@gmail.com>
2024-02-02 13:49:47 +0100

    Navbar done

--- app/App.js
+++ app/App.js
@@ -1,11 +1,24 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import NavBar from './components/navbar';
 
 export default function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NavBar
+        icons={[
+          { label: 'Home', icon: 'home' },
+          { label: 'User', icon: 'user' },
+          { label: 'Settings', icon: 'settings' },
+          { label: 'Bell', icon: 'bell' },
+        ]}
+        selectedIndex={selectedIndex}
+        onPress={(index) => setSelectedIndex(index)}
+      />
     </View>
   );
 }
